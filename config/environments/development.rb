8db854203949037432fe962c1df4b457409cb94b
zacharywolpov <zachary@wolpov.com>
2024-12-04 15:14:58 -0500

    heroku host permissions

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -59,7 +59,7 @@ Rails.application.configure do
   # Suppress logger output for asset requests.
   config.assets.quiet = true
 
-  config.hosts << "testingfashionoutfitsidea-env-1.eba-jhmanytg.us-east-2.elasticbeanstalk.com"
+  config.hosts << "cryptic-headland-46374-86854edcd2d1.herokuapp.com"
 
   # Raises error for missing translations.
   # config.i18n.raise_on_missing_translations = true
